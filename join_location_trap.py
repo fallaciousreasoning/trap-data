211e653c121d678b7b59a262a26da1cf6efc50be
Jay Harris <jay.harris@outlook.co.nz>
2020-08-04 10:59:09 +1200

    Begin matching waypoints

--- join_location_trap.py
+++ join_location_trap.py
@@ -5,10 +5,36 @@ TRAPS_ID="Trap.ID"
 GPX_TRACK_NAME="Holly Craigie traps GPS waypoints 8June20 waypoints"
 
 import gpxpy
+import csv
 
 gpx_data = gpxpy.parse(open(GPX_FILE, 'r'))
 
-for waypoint in gpx_data.waypoints:
-    print(waypoint.name)
+def build_waypoint_dict(waypoints):
+    result = {}
+    for waypoint in waypoints:
+        name = waypoint.name.upper().strip('0')
+        # Note: Some names have leading letters. However, removing them results
+        # in duplicates, so not sure how to treat them yet :/
+        result[name] = waypoint
+    return result
+
+def trap_id(row):
+    trap_line = row[1]
+    trap_id = row[2]
+
+    return trap_id[len(trap_line):]
+
+waypoints = build_waypoint_dict(gpx_data.waypoints)
+
+with open(TRAPS_FILE) as csv_file:
+    reader = csv.reader(csv_file)
+    for index, line in enumerate(reader):
+        if index == 0: continue
+
+        id = trap_id(line)
+        if not id in waypoints:
+            print("Couldn't find a waypoint for", id)
+            continue
+        print(id, waypoints[id])
 # track = get_track(gpx_data)
 # print(track)
